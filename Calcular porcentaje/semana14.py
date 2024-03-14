24453d8df9dc9e1c98c4789482ca9029598ddae5
Marilud1 <cm.castilloc@uea.edu.ec>
2024-03-13 19:28:13 -0500

    13 03 2024

---
+++ Calcular porcentaje/semana14.py
@@ -0,0 +1,6 @@
+#Crea una función llamada calcular_descuento que tome dos parámetros: el monto total de la compra y un valor predeterminado para el porcentaje de descuento (por ejemplo, 10% por defecto).
+
+#La función debe calcular el descuento aplicando el porcentaje al monto total de la compra.
+
+#La función debe devolver el monto del descuento calculado.
+#
